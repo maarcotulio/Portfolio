128006f4bf204f63b9a57d4d8fb39073ec2740ca
maarcotulio <marcotuliotavaresoliveira@gmail.com>
2025-01-01 22:27:18 -0300

    Responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import ChevronDownIcon from "./assets/icons/chevron-down.svg";
 import ReactIcon from "./assets/icons/react.svg";
 import NodeIcon from "./assets/icons/nodejs-icon.svg";
 
-import fincheckImage from "./assets/images/fincheck.png";
-import ankiAddImage from "./assets/images/ankiadd.png";
+import fincheckLowResolution from "./assets/images/fincheckLowResolution.png";
+import ankiAddImage from "./assets/images/ankiadd.svg";
 
 export default function App() {
   function handleClickAnkiAdd() {
@@ -21,7 +21,7 @@ export default function App() {
   return (
     <>
       <div className="h-screen w-full bg-WhiteBackground flex flex-col items-center justify-center relative">
-        <h1 className="font-merriWeather text-5xl">Marco Túlio</h1>
+        <h1 className="font-merriWeather text-5xl text-center">Marco Túlio</h1>
         <p className="mt-4 text-lg">Full Stack Developer</p>
 
         <div className="flex mt-4 gap-4">
@@ -49,7 +49,7 @@ export default function App() {
         <h1 className="text-center text-4xl font-merriWeather tracking-wide font-semibold mb-12">
           Main Skills
         </h1>
-        <div className="flex gap-8 items-center justify-evenly">
+        <div className="flex gap-8 flex-col items-center justify-evenly sm:flex-row">
           <div>
             <img src={ReactIcon} alt="React Icon" className="h-24" />
             <p className="mt-6 tracking-wide text-center  text-2xl">React</p>
@@ -61,21 +61,22 @@ export default function App() {
         </div>
       </div>
 
-      <div className="w-full h-screen bg-WhiteBackground flex flex-col items-center justify-center relative">
-        <h1 className="absolute top-28 text-center text-4xl font-merriWeather tracking-wide font-semibold ">
+      <div className="w-full  bg-WhiteBackground flex flex-col items-center justify-center h-fit">
+        <h1 className="text-center text-4xl mt-28  font-merriWeather tracking-wide font-semibold ">
           Main Projects
         </h1>
 
-        <div className="flex justify-evenly mt-32  w-full">
+        <div className="flex flex-col mt-12 w-full gap-12 items-center justify-center p-12 lg:flex-row lg:justify-evenly">
           <div
-            className="bg-CardBackground rounded-lg overflow-hidden flex flex-col w-96 hover:cursor-pointer hover:scale-105"
+            className="bg-CardBackground rounded-lg overflow-hidden  flex flex-col hover:cursor-pointer hover:scale-105 max-w-96 w-full "
             onClick={handleClickFincheck}
           >
             <img
-              src={fincheckImage}
+              src={fincheckLowResolution}
               alt="Project Image"
-              className="w-96 h-72 object-cover"
+              className="w-96 h-72 "
             />
+
             <div className="p-4 ">
               <h1 className="font-semibold text-lg">Fincheck</h1>
               <p className="text-gray-800">
@@ -87,7 +88,7 @@ export default function App() {
           </div>
 
           <div
-            className="bg-CardBackground rounded-lg overflow-hidden flex flex-col w-96 hover:cursor-pointer hover:scale-105"
+            className="bg-CardBackground rounded-lg overflow-hidden flex flex-col hover:cursor-pointer hover:scale-105 max-w-96 w-full"
             onClick={handleClickAnkiAdd}
           >
             <img
@@ -95,6 +96,7 @@ export default function App() {
               alt="Project Image"
               className="w-96 h-72 object-cover"
             />
+
             <div className=" p-4">
               <h1 className="font-semibold text-lg">AnkiAdd</h1>
               <p className="text-gray-800">
