50a0de7f1a989b5809e414c0eaa08fc557c86690
Marco <marcotuliotavaresoliveira@gmail.com>
2025-06-19 13:29:30 -0300

    Update ESLint configuration to include custom rules and improve compatibility with Next.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { off } from "process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,11 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+    rules: {
+      "prefer-cons": off,
+    },
+  }),
 ];
-
 export default eslintConfig;
